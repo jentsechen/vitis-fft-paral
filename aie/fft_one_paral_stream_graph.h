79a6dc25beb9efb02dec57bb4ebf4ffce0a7a9d4
jentsechen <jentsechen1009@gmail.com>
2025-10-16 14:14:04 +0800

    Add stream mode and expand window size

--- aie/fft_one_paral_stream_graph.h
+++ aie/fft_one_paral_stream_graph.h
@@ -1,10 +1,10 @@
-#ifndef __fft_one_paral_graph__
-#define __fft_one_paral_graph__
+#ifndef __fft_one_paral_stream_graph__
+#define __fft_one_paral_stream_graph__
 
 #include "../../Vitis_Libraries/dsp/L2/include/aie/fft_ifft_dit_1ch_graph.hpp"
 #include <adf.h>
 
-class FftOneParalGraph : public adf::graph {
+class FftOneParalStreamGraph : public adf::graph {
 private:
   xf::dsp::aie::fft::dit_1ch::fft_ifft_dit_1ch_graph<cfloat, // TT_DATA
                                                      cfloat, // TT_TWIDDLE
@@ -15,24 +15,25 @@ private:
                                                      0,      // TP_DYN_PT_SIZE
                                                      1024 *
                                                          2, // TP_WINDOW_VSIZE
-                                                     0,     // TP_API
+                                                     1,     // TP_API
                                                      0,     // TP_PARALLEL_POWER
-                                                     0      // TP_USE_WIDGETS
+                                                     1      // TP_USE_WIDGETS
                                                      >
       fft_kernel;
 
 public:
-  //   adf::input_gmio fft_in;
-  //   adf::output_gmio fft_out;
-  adf::input_plio fft_in;
-  adf::output_plio fft_out;
-  FftOneParalGraph() {
-    // fft_in = adf::input_gmio::create("fft_in", 64, 1000);
-    // fft_out = adf::output_gmio::create("fft_out", 64, 1000);
-    fft_in = input_plio::create("fft_in", adf::plio_64_bits);
-    fft_out = output_plio::create("fft_out", adf::plio_64_bits);
-    adf::connect<>(fft_in.out[0], fft_kernel.in[0]);
-    adf::connect<>(fft_kernel.out[0], fft_out.in[0]);
+  adf::input_plio fft_in_0, fft_in_1;
+  adf::output_plio fft_out_0, fft_out_1;
+  FftOneParalStreamGraph() {
+    fft_in_0 = input_plio::create("fft_in_0", adf::plio_64_bits);
+    fft_in_1 = input_plio::create("fft_in_1", adf::plio_64_bits);
+    fft_out_0 = output_plio::create("fft_out_0", adf::plio_64_bits);
+    fft_out_1 = output_plio::create("fft_out_1", adf::plio_64_bits);
+
+    adf::connect<>(fft_in_0.out[0], fft_kernel.in[0]);
+    adf::connect<>(fft_in_1.out[0], fft_kernel.in[1]);
+    adf::connect<>(fft_kernel.out[0], fft_out_0.in[0]);
+    adf::connect<>(fft_kernel.out[1], fft_out_1.in[0]);
   }
 };
 
